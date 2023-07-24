5e9ca02b72861b4d77ee1c73e2bde3fde6d7abbb
Emir Türk <turkemir2152@gmail.com>
2023-07-24 11:48:46 +0300

    Initial Commit

---
+++ HackerNews/HackerNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackerNewsApp.swift
+//  HackerNews
+//
+//  Created by Emir Türk on 24.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct HackerNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
